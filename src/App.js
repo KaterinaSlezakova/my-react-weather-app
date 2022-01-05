914ad6c31b160f1e715e55b1b64f3d8ababa70b3
Katerina <kslezakova12@gmail.com>
2022-01-05 22:32:51 +0100

    added links

--- src/App.js
+++ src/App.js
@@ -1,12 +1,24 @@
-
-import './App.css';
+import "./App.css";
 
 export default function App() {
   return (
     <div className="App">
-   hello world!
+      <h1>Weather application</h1>
+      <a
+        href="https://github.com/KaterinaSlezakova/my-react-weather-app"
+        target="_blank"
+        rel="noreferrer"
+      >
+        GitHub
+      </a>{" "}
+      and hosted on{" "}
+      <a
+        href="https://amazing-jepsen-eb1ae2.netlify.app/"
+        target="_blank"
+        rel="noreferrer"
+      >
+        Netlify
+      </a>
     </div>
   );
 }
-
-
