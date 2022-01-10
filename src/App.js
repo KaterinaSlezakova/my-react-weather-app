bf338888e633207da3f099353279a93635d5f2e0
Katerina <kslezakova12@gmail.com>
2022-01-10 17:01:39 +0100

    added bootstrap

--- src/App.js
+++ src/App.js
@@ -3,23 +3,28 @@ import "./App.css";
 export default function App() {
   return (
     <div className="App">
-      <h1>Weather application</h1>
-      This project was coded by{" "}
-      <a
-        href="https://www.linkedin.com/in/kslezakova/"
-        target="_blank"
-        rel="noreferrer"
-      >
-        Katerina Slezakova
-      </a>{" "}
-      and is{" "}
-      <a
-        href="https://github.com/KaterinaSlezakova/my-react-weather-app"
-        target="_blank"
-        rel="noreferrer"
-      >
-        opened-sourced on GitHub
-      </a>{" "}
+      <div className="container">
+        <h1>Weather application</h1>
+        <button className="btn btn-primary">ahoj</button>
+        <footer>
+          This project was coded by{" "}
+          <a
+            href="https://www.linkedin.com/in/kslezakova/"
+            target="_blank"
+            rel="noreferrer"
+          >
+            Katerina Slezakova
+          </a>{" "}
+          and is{" "}
+          <a
+            href="https://github.com/KaterinaSlezakova/my-react-weather-app"
+            target="_blank"
+            rel="noreferrer"
+          >
+            opened-sourced on GitHub
+          </a>{" "}
+        </footer>
+      </div>
     </div>
   );
 }
