bc6793e26ca9c23d295897798501c7e29f91817b
Katerina <kslezakova12@gmail.com>
2022-02-07 21:04:05 +0100

    Update FormattedDate.jsx

--- src/components/FormattedDate.jsx
+++ src/components/FormattedDate.jsx
@@ -20,7 +20,7 @@ export default function FormattedDate({ date }) {
  
   return (
       <div>
-          {days[day]} 
+          {days[day]} {""}
           {hours < 10 ? `0${hours}` : hours}
           :
           {minutes < 10 ? `0${minutes}` : minutes}
