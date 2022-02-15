e09b5ce0dc7f18a09d2eac2ba3b5d6de6ec39520
Katerina <kslezakova12@gmail.com>
2022-02-15 17:51:07 +0100

    code moved to new compenent, replaced icon url with dynamic url

--- src/components/Weather.jsx
+++ src/components/Weather.jsx
@@ -1,9 +1,7 @@
 import React, { useState } from "react";
-import Overview from "./Overview";
-import CurrentTemp from "./CurrentTemp";
-import Details from "./Details";
 import axios from "axios";
 import { BallTriangle } from "react-loader-spinner";
+import WeatherInfo from "./WeatherInfo";
 
 import "./Weather.css";
 
@@ -18,12 +16,6 @@ export default function Weather({ defaultCity }) {
   function handleCityChange(e) {
     setCity(e.target.value);
   }
-  function search() {
-    const apiKey = "375b17b86a7f9a868e0d0a3ab5fe16bd";
-    let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
-    axios.get(apiUrl).then(handleResponse);
-  }
-
   function handleResponse(response) {
     setWeatherData({
       loaded: true,
@@ -32,12 +24,19 @@ export default function Weather({ defaultCity }) {
       description: response.data.weather[0].description,
       humidity: response.data.main.humidity,
       wind: response.data.wind.speed,
-      iconUrl: "https://ssl.gstatic.com/onebox/weather/64/partly_cloudy.png",
+      iconUrl: `http://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`,
       time: new Date(response.data.dt * 1000),
       sunrise: new Date(response.data.sys.sunrise * 1000),
       sunset: new Date(response.data.sys.sunset * 1000),
     });
   }
+
+  function search() {
+    const apiKey = "375b17b86a7f9a868e0d0a3ab5fe16bd";
+    let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
+    axios.get(apiUrl).then(handleResponse);
+  }
+
   if (weatherData.loaded) {
     return (
       <div className="Weather">
@@ -61,30 +60,9 @@ export default function Weather({ defaultCity }) {
             </div>
           </div>
         </form>
-        <div className="row">
-          <div className="col-4">
-            <CurrentTemp
-              temperature={weatherData.temperature}
-              icon={weatherData.iconUrl}
-              description={weatherData.description}
-            />
-          </div>
-          <div className="col-3">
-            <Details
-              wind={weatherData.wind}
-              humidity={weatherData.humidity}
-              sunrise={weatherData.sunrise}
-              sunset={weatherData.sunset}
-            />
-          </div>
-          <div className="col-4 text-end">
-            <Overview
-              city={weatherData.city}
-              description={weatherData.description}
-              time={weatherData.time}
-            />
-          </div>
-        </div>
+        <WeatherInfo
+          data={weatherData}
+        />
       </div>
     );
   } else {
