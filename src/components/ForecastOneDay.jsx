f3eb5a8ee6345181578a983482bdbb68145e8eeb
Katerina S <kslezakova12@gmail.com>
2026-03-16 21:20:56 +0100

    fixing bugs in functions regarding data from api

--- src/components/ForecastOneDay.jsx
+++ src/components/ForecastOneDay.jsx
@@ -3,27 +3,26 @@ import WeatherIcon from "./WeatherIcon";
 
 export default function ForecastOneDay(props) {
   function maxTemp() {
-    let temp = Math.round(props.data.temp.max);
-    return `${temp}°`;
+  return Math.round(props.data.main.temp_max);
+    
   }
   function minTemp() {
-    let temp = Math.round(props.data.temp.min);
-    return `${temp}°`;
+    return Math.round(props.data.main.temp_min);
   }
+    
   function day() {
-    let date = new Date(props.data.dt * 1000);
-    let day = date.getDay();
-    let days = ["Sun", "Mon", "Thu", "Wed", "Thur", "Fri", "Sat"];
-
-    return days[day];
+    const date = new Date(props.data.dt_txt);
+    const days = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
+    
+    return days[date.getDay()];
   }
   return (
     <div className="ForecastOneDay">
       <div className="Forecast-day">{day()}</div>
       <WeatherIcon code={props.data.weather[0].icon} size={36} />
       <div className="Forecast-temperatures">
-        <span className="Forecast-max-temp">{maxTemp()}</span>
-        <span className="Forecast-min-temp">{minTemp()}</span>
+        <span className="Forecast-max-temp">{maxTemp()}°</span>
+        <span className="Forecast-min-temp">{minTemp()}°</span>
       </div>
     </div>
   );
