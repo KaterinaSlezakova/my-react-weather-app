4c4ad1aeb90797137a2d7f6cf7e489aa795e679c
Katerina S <kslezakova12@gmail.com>
2026-03-29 17:36:42 +0200

    Add VisitorCounter in App.jsx

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import React from "react";
 import "./App.css";
 import "react-loader-spinner/dist/loader/css/react-spinner-loader.css";
 import Weather from "./components/Weather/Weather";
+import VisitorCounter from "./components/VisitorCounter";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faLinkedin, faGithub } from "@fortawesome/free-brands-svg-icons";
 
@@ -32,6 +33,7 @@ export default function App() {
             open sourced on <FontAwesomeIcon icon={faGithub} />
           </a>{" "}
         </footer>
+        < VisitorCounter />
       </div>
     </div>
   );
